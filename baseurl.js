96a02375b891f5df5d554033906f8d580286ffed
shiak <shaikparveez290@gmail.com>
2026-04-19 12:30:43 +0530

    added the env file

--- baseurl.js
+++ baseurl.js
@@ -1,4 +1,6 @@
 //url from the render
 // export const baseUrl = 'https://image-viewer-1.onrender.com'
 //url from the backend
-export const baseUrl = 'http://localhost:5000'
+// export const baseUrl = 'http://localhost:5000'
+
+export const baseUrl = import.meta.env.VITE_BASE_URL
