6c8c6a44eb6eb642d57fe11d5e1dda6a4acd180a
shiak <shaikparveez290@gmail.com>
2026-04-21 22:13:12 +0530

    need to change the fully adminDahsboard

--- src/baseUrl.js
+++
@@ -1,6 +0,0 @@
-//url from the render
-// export const baseUrl = 'https://image-viewer-1.onrender.com'
-//url from the backend
-// export const baseUrl = 'http://localhost:5000'
-
-export const baseUrl = import.meta.env.VITE_BASE_URL
